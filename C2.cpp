4ba862174bea31fb8b133f63a4661777ed380104
Eos <86279093+JosueEnrique@users.noreply.github.com>
2025-12-03 17:51:45 -0600

    Implement iterative Tower of Hanoi solution
    Added an iterative version of the Tower of Hanoi algorithm and a Movimiento struct to track moves.

--- C2.cpp
+++ C2.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
+#include <stack>
+#include <cmath>
 using namespace std;
 
-void hanoi(int n, char origen, char destino, char auxiliar) {
-    if (n == 1) {
-        cout << "Mover disco 1 de " << origen << " a " << destino << endl;
-        return;
-    }
-    hanoi(n - 1, origen, auxiliar, destino);
-    cout << "Mover disco " << n << " de " << origen << " a " << destino << endl;
-    hanoi(n - 1, auxiliar, destino, origen);
-}
+struct Movimiento {
+    int disco;
+    char desde;
+    char hacia;
+};
 
-int main() {
-    int discos;
-    cout << "Ingrese el número de discos: ";
-    cin >> discos;
-    hanoi(discos, 'A', 'C', 'B'); // A = origen, C = destino, B = auxiliar
-    return 0;
+void hanoiIterativo(int n, char origen, char destino, char auxiliar) {
+    stack<Movimiento> pila;
+    int totalMovimientos = pow(2, n) - 1;
+
+    if (n % 2 == 0) swap(destino, auxiliar);
+
+    for (int i = 1; i <= totalMovimientos; i++) {
+        int disco = log2(i & -i) + 1;
+
+        if (i % 3 == 1)
+            cout << "Mover disco " << disco << " de " << origen << " a " << destino << endl;
+        else if (i % 3 == 2)
+            cout << "Mover disco " << disco << " de " << origen << " a " << auxiliar << endl;
+        else
+            cout << "Mover disco " << disco << " de " << auxiliar << " a " << destino << endl;
+    }
 }
